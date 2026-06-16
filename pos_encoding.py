60311c9ebdb0540e103b7519436a02285c9e0d12
0xIta3hi <aryanbharadiyak2006@gmail.com>
2026-06-16 23:28:34 +0530

    pipeline for getting positional encoding

---
+++ pos_encoding.py
@@ -0,0 +1,9 @@
+import torch
+import math
+
+def get_positional_encoding(seq_len, d_model):
+    pass
+
+if __name__ == "__main__":
+    pe_matrix = get_positional_encoding(seq_len=10, d_model=16)
+    print("Positional Encoding Shape:", pe_matrix.shape)
