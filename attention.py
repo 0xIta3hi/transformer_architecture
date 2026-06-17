34304d8939cec5618666016f76a0b93e59526727
0xIta3hi <aryanbharadiyak2006@gmail.com>
2026-06-17 13:31:18 +0530

    optimized the forward function

--- attention.py
+++ attention.py
@@ -3,21 +3,29 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class selfAttention:
-    def __init__(self, d_model):
-        super().__init__()
-        self.d_model = d_model
+    def __init__(self, d_k):
+        self.d_k = d_k
 
-        self.w_q = nn.Linear(d_model,d_model, bias=False)
-        self.w_k = nn.Linear(d_model, d_model, bias=False)
-        self.w_v = nn.Linear(d_model,d_model,bias=False)
-
-    def forward(self, x, mask=None):
+        self.w_q = nn.Linear(d_k, d_k, bias=False)
+        self.w_k = nn.Linear(d_k, d_k,bias=False)
+        self.w_v = nn.Linear(d_k,d_k,bias=False)
+    
+    def forward(self, x, d_k):
         """
-        x shape: [batch_size, seq_len, d_model].
+        x shape: [batch_size, seq_len, d_k]
         """
-        batch_size, seq_len, d_model = x.shape
+        batch_size, seq_len, d_k = x.shape
 
-        Q = self.w_q(x)
-        K = self.w_k(x)
-        V = self.w_v(x)
-        
+        Q = self.w_q @ x
+        K = self.w_k @ x
+        V = self.w_v @ x
+
+        scores = torch.matmul(Q,K.transpose(-2,-1))
+
+        scores = scores / (self.d_k ** 0.5)
+
+        attention_weight = F.softmax(scores, dim=-1)
+        output = attention_weight @ V
+
+        return output, attention_weight
+    
