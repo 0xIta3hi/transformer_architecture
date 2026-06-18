b6562f4f6eb8a2b24e75d98a56f620d4f2124cba
0xIta3hi <aryanbharadiyak2006@gmail.com>
2026-06-18 13:39:24 +0530

    wrote multi-head attention

--- multi_head_attention.py
+++ multi_head_attention.py
@@ -4,10 +4,41 @@ import torch.nn.functional as F
 
 class MultiHeadAttention:
     def __init__(self, d_k, d_head):
+        assert self.d_k % d_head == 0
         self.d_k = d_k
         self.d_head = d_head
-
+        self.d_model = d_k // d_head
         self.w_q = nn.Linear(d_k,d_k,bias=False)
         self.w_k = nn.Linear(d_k,d_k,bias=False)
         self.w_v = nn.Linear(d_k,d_k,bias=False)
+        self.w_o = nn.Linear(d_k,d_k,bias=False) # final projection layer
+    
+    def forward(self, x, mask=None):
+        B,T, d_k = x.shape
+
+        Q = self.w_q @ x
+        K = self.w_k @ x
+        V = self.w_v @ x
+
+        # now we split these
+        q = q.view(B, T, self.num_heads, self.d_k).permute(0, 2, 1, 3)
+        k = k.view(B, T, self.num_heads, self.d_k).permute(0, 2, 1, 3)
+        v = v.view(B, T, self.num_heads, self.d_k).permute(0, 2, 1, 3)
+
+        score = torch.matmul(q,k.transpose(-2,-1)) / (self.d_k ** 0.5)
+
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, float("-inf"))
+
+        attention_weights = F.softmax(score, dim=-1)
+
+        output = attention_weights @ v
+
+        context = context.purmute(0,2,1,3).contiguous()
+
+        concat_output = context.view(B, T, self.d_model)
+        
+        # 5. Final linear projection mix
+        return self.W_o(concat_output), attention_weights
+    
         
