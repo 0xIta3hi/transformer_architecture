208bfd8e7b69f76c4da4222c0b5dfb1d8c88449e
0xIta3hi <aryanbharadiyak2006@gmail.com>
2026-06-23 00:54:01 +0530

    implemented forward function

--- ffn.py
+++ ffn.py
@@ -9,5 +9,12 @@ class FeefForwardNetwork(nn.Module):
 
         self.down_proj = nn.Linear(d_k, 2*d_ff, bias=False)
 
-    
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
         
+        combined_projections = self.fused_gate_up(x)
+        gate_track, up_track = combined_projections.chunk(2, dim=-1)
+        gated_hidden_states = F.silu(gate_track) * up_track
+        output = self.down_proj(gated_hidden_states)
+        return output
+    
