86f484c8a8859299e2ce758caa31d034c45bd729
0xIta3hi <aryanbharadiyak2006@gmail.com>
2026-06-23 00:18:52 +0530

    implemented initialization function

--- ffn.py
+++ ffn.py
@@ -2,6 +2,12 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as F
 
-class FeefForwardNetwork:
-    def __init__(self):
-        pass
+class FeefForwardNetwork(nn.Module):
+    def __init__(self, d_k:int, d_ff:int):
+
+        self.fused_gate_up = nn.Linear(d_k, 2*d_ff, bias=False)
+
+        self.down_proj = nn.Linear(d_k, 2*d_ff, bias=False)
+
+    
+        
