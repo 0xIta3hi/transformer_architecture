966b71289d2c37b0e0285de6b1a2addfbf23ae45
0xIta3hi <aryanbharadiyak2006@gmail.com>
2026-06-29 22:52:58 +0530

    wrote swiglu forward function

--- swiglu.py
+++ swiglu.py
@@ -3,4 +3,9 @@ import torch.nn as nn
 
 class SwigLu(nn.Module):
     def __init__():
-        pass
+        super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x * torch.sigmoid(x)
+    
+    
